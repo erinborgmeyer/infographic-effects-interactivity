4b45c3556eff7ea4378eb32ba60a05ff83de4d2b
erinborgmeyer <borg0027@algonquinlive.com>
2016-04-29 17:07:52 -0400

    more content

--- js/main.js
+++ js/main.js
@@ -1,21 +1,19 @@
 var $svg = $('svg');
+var $li = $('<li>');
+var $figure = $('<figure>');
+var $img = $('<img>');
+var $caption = $('<figcaption>');
+var $ul = $('ul');
 
 $('.btn').on('click', function () {
   $svg.toggleClass('is-facts');
 });
-
-
-var $ul = $('ul');
 
 $ul.addClass('nurses-list');
 
 $('.more-nurses').remove();
 
 nurses.forEach(function (nurses) {
-  var $li = $('<li>');
-  var $figure = $('<figure>');
-  var $img = $('<img>');
-  var $caption = $('<figcaption>');
 
   $caption.html(nurses.name);
   $img.attr('src', 'images/' + nurses.img);
